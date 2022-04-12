f65e73c2b4964b79c1e43c67e4d7682fceab341b
ttorbeynS <ttorbeyn@student.s19.be>
2022-04-12 13:51:40 +0200

    save

--- pipex.c
+++ pipex.c
@@ -2,6 +2,10 @@
 
 int	main(int ac, char **av)
 {
+	if (ac != 5)
+		exit (0);
+	else
+
 	printf("coucou\n");
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -1,6 +1,13 @@
 #ifndef PIPEX_H
 #define PIPEX_H
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/wait.h>
+
 
 #endif
